4cdbe9cd835792c3ae465e198201d1de16807221
Alla Alenova <alla_alenova@mail.ru>
2020-08-15 23:50:46 +0300

    added @order

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,36 +1,38 @@
 package ru.netology.web.test;
 
 import lombok.val;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 class MoneyTransferTest {
 
-
   @Test
-  void shouldTransferMoneyBag() {
+  @Order(2)
+  void shouldErrorIfBalanceCardBelowZero() {
     String sum = "30000";
     val loginPage = open("http://localhost:9999", LoginPage.class);
     val authInfo = DataHelper.getAuthInfo();
     val verificationPage = loginPage.validLogin(authInfo);
     val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
     val transferPage = verificationPage.validVerify(verificationCode);
-    int balanceCard1 = transferPage.getCard1Balance();
-    int balanceCard2 = transferPage.getCard2Balance();
     val cardPageReplenish = transferPage.replenish1();
     val infoCard = DataHelper.getCardNumber2();
     cardPageReplenish.replenishCardToCard(infoCard, sum);
-    assertEquals(balanceCard1 + Integer.parseInt(sum), transferPage.getCard1Balance());
-    assertEquals(balanceCard2 - Integer.parseInt(sum), transferPage.getCard2Balance());
+    assertEquals("Недостаточно средств на карте.", cardPageReplenish.errorMessage1());
   }
 
   @Test
-  void shouldTransferMoney() {
+  @Order(1)
+  void shouldATransferMoney() {
     String sum = "10000";
     val loginPage = open("http://localhost:9999", LoginPage.class);
     val authInfo = DataHelper.getAuthInfo();
